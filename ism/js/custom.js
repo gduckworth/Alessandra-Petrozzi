fa004e948034cff32721d38582b9d1e0b6240417
gduckworth1 <44668059+gduckworth1@users.noreply.github.com>
2019-09-14 15:41:27 +0100

    update

---
+++ ism/js/custom.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '16651177378',
+	limit :12
+        accessToken:'16651177378.1677ed0.aff54e0e78dd4cfc9b70250e859b4ba3',
+	sortBy: 'most-recent'
+	templatw: '<div class-"gallery"><a href-"{{image}}" title-"{{caption}}" target-"_blank"><img src-"{{image}}" alt-"{{caption}}" class-"img-fluid/></a></div>,
+    });
+    userFeed.run();
+
+
+});
+</script>
